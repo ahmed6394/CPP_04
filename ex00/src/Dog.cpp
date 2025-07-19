2db85b455141e523e7610a73f4dcfea592787d28
Mahabub Ahmed <mahabub.ahmed.eee@gmail.com>
2025-07-19 15:28:05 +0200

    dog class added

--- ex00/src/Dog.cpp
+++ ex00/src/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: gahmed <gahmed@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 13:47:07 by gahmed            #+#    #+#             */
-/*   Updated: 2025/07/19 13:51:57 by gahmed           ###   ########.fr       */
+/*   Updated: 2025/07/19 15:23:33 by gahmed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,39 @@
 Dog::Dog() : Animal()
 {
 	type = "Dog";
-	
+	std::cout << "Dog default constructor called\n";
 }
 
 // perameterise constructor
 Dog::Dog(std::string aType) : Animal()
 {
-	
+	type = aType;
+	std::cout << "Dog perameterise constructor called\n";
 }
 
 // copy constructor
-Dog::Dog(const Dog& obj) : Animal()
+Dog::Dog(const Dog& obj) : Animal(obj)
 {
-	
+	*this = obj;
+	std::cout << "Dog copy constructor called\n";
 }
 
 // copy assingment operator
 Dog& Dog::operator=(const Dog& obj)
 {
-	
+	if(this != &obj)
+		this->type = obj.type;
+	std::cout << "Dog assignment operator called\n";
 }
 
 //destructor
 Dog::~Dog()
 {
-	
+	std::cout << "Dog destructor called\n";
 }
 
+// override function
 void Dog::makeSound() const
 {
-	
+	std::cout << "Woof Woof\n";
 }
